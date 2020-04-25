a915bb7cdeb8db25ad35115f8e078408fde689a4
drogokaya <adityachettri51@gmail.com>
2020-04-26 02:13:33 +0530

    Updates

---
+++ precache-manifest.5a2d431838e3231d1a28557892a852ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986c2c965dead5238e16d24d7caa32df",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "882041116782a8cef334",
+    "url": "/assignment/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "0b0da8d66b208faa8358",
+    "url": "/assignment/static/css/main.71ae691c.chunk.css"
+  },
+  {
+    "revision": "882041116782a8cef334",
+    "url": "/assignment/static/js/2.341fc9dd.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/assignment/static/js/2.341fc9dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0da8d66b208faa8358",
+    "url": "/assignment/static/js/main.3c93a561.chunk.js"
+  },
+  {
+    "revision": "f3a370a0e17119ebd3b9",
+    "url": "/assignment/static/js/runtime-main.da855f98.js"
+  }
+]);
